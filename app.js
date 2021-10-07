aafe9c2a13ff526ba68533962c65f62147f7b81e
moody94 <moody.kh.022@gmail.com>
2021-10-07 13:18:55 +0100

    kmom02

--- app.js
+++ app.js
@@ -9,19 +9,17 @@ const middleWare = require("./middlware/errorhandel");
 const hello = require("./routes/hello");
 const items = require("./routes/get_items");
 
+app.use(cors());
+
+app.use(middleWare.middleWare);
 app.use("/hello", hello);
 app.use("/", items);
 
-app.use(cors());
-app.options('*', cors())
 app.use(morgan());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-app.use(middleWare.middleWare),
-  app.use(middleWare.notFoundError),
-  app.use(middleWare.errorResult);
+app.use(middleWare.notFoundError), app.use(middleWare.errorResult);
 
 if (process.env.NODE_ENV !== "test") {
   app.use(morgan("combined"));
